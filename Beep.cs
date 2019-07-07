334273e40b86a5333c15167167ff0fd3285d7a86
Yuuki Wesp <ls-micro@ya.ru>
2019-07-08 01:33:23 +0300

    Create Beep.cs

---
+++ Beep.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using ancient.runtime;
+using static System.Console;
+
+
+[Guid("15A2AE41-975E-4FEA-9555-FBD380DB845B")]
+public class BeepDevice : Device
+{
+    public override void write(long address, long data)
+    {
+        switch (address)
+        {
+            case 0xF:
+                Console.Beep(2400, 1000)
+                break;
+        }
+    }
+
+    public override void shutdown() {}
+    public TerminalDevice() : base(0x5, "beep") { }
+}
